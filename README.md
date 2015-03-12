68e6e66e1f6492cf8fa7965528dfde9575c4942e
Ye Feng <feng_ye2001@hotmail.com>
2015-03-12 22:54:22 +0800

    Add initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MyoMouse
 An extreme simple script to drive your mouse with Myo armband
+
+Installation
+============
+Install the myomouse.lua script using Myo Connect->Application Manager->Add
+
+
+Usage
+=====
+1. Move arm to move the mouse cursor
+2. Fingers Spread guesture to perform left click
+3. Keep fist guesture to perform drag, release fist guesture to perform drop
+4. Double tap gesture to perform double click
---
+++ myomouse.lua
@@ -0,0 +1,52 @@
+scriptId = 'me.beaconguide.myomouse'
+scriptTitle = "Myo Mouse"
+scriptDetailsUrl = "https://github.com/fengye/MyoMouse"
+
+myo.controlMouse(true)
+myo.setLockingPolicy('none')
+-- say hello world
+
+
+function activeAppName()
+    return "Myo Mouse"
+end
+
+function onPoseEdge(pose, edge)
+    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+
+    if pose == 'fingersSpread' and edge == 'on' then
+    	myo.mouse('left', 'click')
+    	myo.vibrate('short')
+    elseif pose == 'fist' then
+    	if edge == 'on' then
+    		myo.mouse('left', 'down')
+    		myo.vibrate('medium')
+    	else
+    		myo.mouse('left', 'up')
+    		myo.vibrate('short')
+    	end
+    elseif pose == 'doubleTap' then
+    	-- emulate double click?
+    	myo.mouse('left', 'click')
+	elseif pose == 'rest' then
+		myo.mouse('left', 'up')
+	end
+end
+
+function onPeriodic()
+end
+
+
+function onForegroundWindowChange(app, title)
+    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    return true
+end
+
+
+function onActiveChange(isActive)
+    myo.debug("onActiveChange")
+
+    if isActive then
+    	myo.vibrate('long')
+    end
+end
